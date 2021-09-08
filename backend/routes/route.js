f168002132bb33b31771d80d07a92c1cb43597c2
Ion-Abirami <abirami@unosales.com>
2021-09-08 11:15:26 +0530

    register

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -9,9 +9,4 @@ router.put("/form/:id", registerController.updateReg);
 router.delete("/form/:id", registerController.deleteReg);
 
 
-router.get("/form", registerController.readReg);
-router.post("/form", registerController.addReg);
-router.put("/form/:id", registerController.updateReg);
-router.delete("/form/:id", registerController.deleteReg);
-
 module.exports = router;
